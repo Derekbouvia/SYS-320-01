0fdfdf1944a26ee6806f3c922f973c7bbca656f3
Derekbouvia <70984955+Derekbouvia@users.noreply.github.com>
2023-02-15 21:57:16 -0500

    Update manage-users.bash

--- Week 3/manage-users.bash
+++ Week 3/manage-users.bash
@@ -1,151 +1,55 @@
-#!/bin/bash 
+#!/bin/bash
 
-#Storyline: Menu for admin, VPN, and security functions
-function invalid_opt() {
+#Storyline: Script to add and delete VPN peers
 
-	echo ""
-	echo "Invalid Option"
-	echo ""
-	sleep 2
+while getopts 'hdau:' OPTION ; do
 	
-
-}
-function menu() {
-
-	#clears the screen
-	clear
-
-	echo "[1] Admin Menu"
-	echo "[2] Security Menu"
-	echo "[3] Exit"
-	read -p "Please enter a choice above: "  choice
-
-	case "$choice" in 
-
-		1) admin_menu
-		;;
-
-		2) security_menu
-		;;
-		
-		3) exit 0
-		;;
-		
-		*) 
-			invalid_opt			
-			# Call the main menu
-			menu
-		;;
+	case "$OPTION" in
+		d) u_del=${OPTION} ;;
+		a) u_add=${OPTION} ;;
+		u) t_user=${OPTARG} ;; 
+		h)
+			echo ""
+		       	echo "Usage: $(basename $0) [-a] | [-d] -u username" 
+			echo ""
+			exit 1 ;;
+		*)  
+			echo "Invalid value."
+			exit 1
+			;;
 
 	esac
 
-
-}
-function admin_menu() {
-
-	clear	
-	echo "[L]ist Running processes"
-	echo "[N]etwork sockets"
-	echo "[V]PN Menu"
-	echo "[4] Exit"
-	read -p "Please enter a choice above: " choice
-
-	case "$choice" in
-
-		L|l)
-			ps -ef | less
-		;;
-
-		N|n)
-			netstat -an --inet | less
-		;;
-
-		V|v) 
-			vpn_menu
-		;;
-
-		4) exit 0
-		
-		;;
-
-		*)
-			invalid_opt
-
-			admin_menu
-		;;
+done
 
 
-	esac
+# Check to see if the -a and -d are empty or if they are both specified throw an error
+if [[ (${u_del} == "" && ${u_add} == "") || (${u_del} != "" && ${u_add} != "") ]]
+then
+	echo "Please specify -a or -d and the -u and username."
+fi
 
 
-admin_menu
-}
+# Check to ensure -u is specified
 
-function security_menu(){
-	clear 
-	echo "[1] list open sockets"
-	echo "[2] Check users with UID of 0"
-	echo "[3] Check last 10 logged in users"
-	echo "[4] Current logged in users"
-	echo "[5] Main menu"
-	echo "[6] Exit"
-	read -p "Please enter a choice above: " choice
-
-	case "$choice" in
-		1) netstat -an --inet | less
-			;;
-		2) cat /etc/passwd | grep 0:0 | less
-			;;
-		3) last | less
-			;;
-		4) w | less
-			;;
-		5) menu
-			;;
-		6) exit 0
-			;;
-		*)
-			invalid_opt
-			
-			;;
-	esac
+if [[ (${u_del} != "" || ${u_add} != "") && ${t_user} == "" ]]
+then
+	echo "Please specify a user (-u)!"
+	echo "Usage: $(basename $0) [-a][-d] [-u username]"
+	exit 1
+fi
 
 
+# Delete a user
+if [[ ${u_del} ]]
+then
+	echo "Deleting user..."
+	sed -i "/# ${t_user} begin/,/# ${t_user} end/d" /etc/wireguard/wg0.conf
+fi
 
-security_menu
-}
-function vpn_menu() {
-	clear
-	echo "[A]dd a peer"
-	echo "[D]elete a peer"
-	echo "[B]ack to main menu"
-	echo "[M]ain menu"
-	echo "[E]xit"
-	read -p "Please select an option: " choice
-
-	case "$choice" in
-		A|a) bash peer.bash 
-			;;
-		D|d) echo "Please enter a user you would like to delete..."
-			read deluser	
-			bash manage-users.bash -d -u ${deluser}
-			;;
-		B|b) admin_menu
-			;;
-		M|m) menu
-			;;
-		E|e) exit 0
-			;;
-		*)
-			invalid_opt
-			
-			;;
-	esac 
-
-
-
-vpn_menu
-}
-
-#Call the main function
-menu
+# Add a user
+if [[ ${u_add} ]]
+then
+	echo "Create the User..."
+	bash peer.bash ${t_user}
+fi
