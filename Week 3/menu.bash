3aaf5039c93b14a9c2f592a6ce17e72917870259
Derekbouvia <70984955+Derekbouvia@users.noreply.github.com>
2023-02-15 21:40:46 -0500

    Update menu.bash

--- Week 3/menu.bash
+++ Week 3/menu.bash
@@ -1,153 +1,151 @@
-#!/bin/bash
+#!/bin/bash 
 
-#  Storyline: Menu for admin, VPN, and Security functions
-function invalid_opt()  {
+#Storyline: Menu for admin, VPN, and security functions
+function invalid_opt() {
 
-        echo ""
-        echo "Invalid option!"
-        echo ""
-        sleep 2
-        
-      
+	echo ""
+	echo "Invalid Option"
+	echo ""
+	sleep 2
+	
+
 }
 function menu() {
 
-         # clears the screen
-         clear
+	#clears the screen
+	clear
 
-         echo "[1] Admin Menu"
-         echo "[2] Security Menu"
-         echo "[3] Exit"                                                                                                                    menu.bash                                                                                                                          I   S  
-         read -p "Please enter a choice above: " choice
- 
-         case "$choice" in
- 
-                 1) admin_menu
-                 ;;
- 
-                 2) security_menu
-                 ;;
- 
-                 3) exit 0
-                 ;;
- 
-                 *)
-                        invalid_opt
-                        # Call the main menu
-                        menu
-                 ;;
- 
-          esac
- 
- 
+	echo "[1] Admin Menu"
+	echo "[2] Security Menu"
+	echo "[3] Exit"
+	read -p "Please enter a choice above: "  choice
+
+	case "$choice" in 
+
+		1) admin_menu
+		;;
+
+		2) security_menu
+		;;
+		
+		3) exit 0
+		;;
+		
+		*) 
+			invalid_opt			
+			# Call the main menu
+			menu
+		;;
+
+	esac
+
+
 }
-function admin_menu()  {
+function admin_menu() {
 
-        clear
-        echo "[L]ist Running Processes"
-        echo "[N]etwork sockets"
-        echo "[V]PN Menu"
-        echo "[4] Exit"
-        read -p "Please enter a choice above: " choice
+	clear	
+	echo "[L]ist Running processes"
+	echo "[N]etwork sockets"
+	echo "[V]PN Menu"
+	echo "[4] Exit"
+	read -p "Please enter a choice above: " choice
 
-        case "$choice" in
- 
-                L|l)
-                        ps -ef | less
-                ;;
-                  
-                N|n)
-                        netstat -an --inet | less
-                ;;
+	case "$choice" in
 
-                V|v)
-                         vpn_menu
-                ;;
- 
-                4) exit 0
- 
-                ;;                                                                                                                      menu.bash                                                                                                                          I   S  
-                
- 
-                *)
-                        invalid_opt
+		L|l)
+			ps -ef | less
+		;;
 
-                        admin_menu
-                ;;
- 
- 
-    esac
- 
- 
+		N|n)
+			netstat -an --inet | less
+		;;
+
+		V|v) 
+			vpn_menu
+		;;
+
+		4) exit 0
+		
+		;;
+
+		*)
+			invalid_opt
+
+			admin_menu
+		;;
+
+
+	esac
+
+
 admin_menu
 }
- 
+
 function security_menu(){
-        clear
-        echo "[1] list open sockets"
-        echo "[2] Check users with UID of 0"
-        echo "[3] Check last 10 logged in users"
-        echo "[4] Current logged in users"
-        echo "[5] Main Menu"
-        echo "[6] Exit"
-        read -p "Please enter a choice above: * choice
- 
-        case "$choice" in 
-                1) netstat -an --inet | less
-                        ;;
-                2) cat /etc/passwd | grep 0:0 | less
-                        ;;
-                3) last | less
-                        ;;
-                4) w | less
-                        ;;
-                5) menu
-                        ;;
-                6) exit 0
-                        ;;
-                 *)
-                        invalid_opt
+	clear 
+	echo "[1] list open sockets"
+	echo "[2] Check users with UID of 0"
+	echo "[3] Check last 10 logged in users"
+	echo "[4] Current logged in users"
+	echo "[5] Main menu"
+	echo "[6] Exit"
+	read -p "Please enter a choice above: " choice
 
-                        ;;
-    esac
+	case "$choice" in
+		1) netstat -an --inet | less
+			;;
+		2) cat /etc/passwd | grep 0:0 | less
+			;;
+		3) last | less
+			;;
+		4) w | less
+			;;
+		5) menu
+			;;
+		6) exit 0
+			;;
+		*)
+			invalid_opt
+			
+			;;
+	esac
 
 
 
-security_menu                                                                                                                       menu.bash                                                                                                                          I   S  
+security_menu
 }
 function vpn_menu() {
-        clear
-        echo "[A]dd a peer"
-        echo "[D]elete a peer"
-        echo "[B]ack to admin menu"
-        echo "[M]ain Menu"
-        echo "[E]xit"
-        read -p "Please select an option: " choice
-        
-        case "$choice" in
-                A|a) bash peer.bash
-                 tail -6 wg0.conf | less
-                        ;;
-                D|d) echo "Please enter a user you would like to delete..."
-                        read {deluser}
-                        bash manage-users.bash -d -u ${deluser}
-                        ;;
-                B|b) admin_menu
-                        ;;
-                M|m) menu
-                        ;;
-                E|e) exit 0
-                        ;;
-                *)
-                         invalid_opt
+	clear
+	echo "[A]dd a peer"
+	echo "[D]elete a peer"
+	echo "[B]ack to main menu"
+	echo "[M]ain menu"
+	echo "[E]xit"
+	read -p "Please select an option: " choice
 
-                        ;;
-    esac
+	case "$choice" in
+		A|a) bash peer.bash 
+			;;
+		D|d) echo "Please enter a user you would like to delete..."
+			read deluser	
+			bash manage-users.bash -d -u ${deluser}
+			;;
+		B|b) admin_menu
+			;;
+		M|m) menu
+			;;
+		E|e) exit 0
+			;;
+		*)
+			invalid_opt
+			
+			;;
+	esac 
 
 
 
 vpn_menu
 }
 
-# Call the main function
+#Call the main function
 menu
